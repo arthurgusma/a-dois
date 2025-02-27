1c81ae2cc97cf376d760f6009fc0d0dfb2df28c3
gusmadev <arthur.cabj@hotmail.com>
2025-02-27 10:44:01 -0300

    fix: removes unused pages and imports

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,17 @@
-import { router } from "expo-router";
-import { Text, View } from "react-native";
+import { View } from "react-native";
 import Header from "@/components/Header";
-import { Button } from "@/components/ui/button";
+import Search from "@/components/Search";
+import Tracker from "@/components/Tracker";
 
 export default function Index() {
     return (
         <View style={{ flex: 1, padding: 40, gap: 40 }}>
             <Header />
-            <View >
-                {/* <Link href="/(tabs)" style={styles.title}>Dashboard</Link> */}
-                <Button onPress={() => router.navigate("/home")}>
-                    <Button.Title>Começar</Button.Title>
-                </Button>
+            <Search />
+            <View style={{flex: 1, paddingVertical: 40, gap: 40 }}>
+                <Tracker title='Planos' />
+                <Tracker title='Hábitos' />
+                <Tracker title='Datas' />
             </View>
         </View>
     )

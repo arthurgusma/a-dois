87257946bbbf6fd51ddd4e3582d7a72f31faba50
gusmadev <arthur.cabj@hotmail.com>
2025-03-01 12:02:56 -0300

    feat: v1 of zustand with inital store

--- lib/zustand/store.ts
+++ lib/zustand/store.ts
@@ -1,1 +1,18 @@
- 
+import { Habit, User } from '@/types/global'
+import { create } from 'zustand'
+
+type Store = {
+    user: User
+    habits: Habit[]
+    setUser: (user: User) => void
+    setHabits: (habits: Habit[]) => void
+    addNewHabit: (habit: Habit) => void
+}
+
+export const useStore = create<Store>()((set) => ({
+    user: {} as User,
+    habits: [],
+    setUser: (user: User) => set({ user: user}),
+    setHabits: (habits: Habit[]) => set({ habits: habits}),
+    addNewHabit: (habit: Habit) => set((state) => ({ habits: { habit, ...state.habits }})),
+}))
